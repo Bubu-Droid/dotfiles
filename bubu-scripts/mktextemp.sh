ba143f370929e5de46362fb10d62ae05b61622c7
Bubu-Droid <fufudadw@gmail.com>
2024-05-15 20:29:16 +0530

    Update dotfiles and make scripts

---
+++ bubu-scripts/mktextemp.sh
@@ -0,0 +1,6 @@
+echo "Creating LaTeX template..."
+
+mkdir -p ./images
+cp ~/texmf/tex/latex/template.tex ./
+
+echo "LaTeX template has been successfully created!"
---
+++ bubu-scripts/supdate.sh
@@ -0,0 +1,10 @@
+sudo pacman -Syu
+yay -Sua
+tlmgr update --all
+sudo pacman -Qneq > ~/pacman-Qneq.txt
+sudo pacman -Qmeq > ~/pacman-Qmeq.txt
+
+cat /home/bubu/Desktop/.gitignore
+echo "DOWNLOADS"
+echo "DESKTOP"
+echo "Update all the git stuff here"
