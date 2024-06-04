3d92035e50b67b653b405daee90b83858583b223
Bubu-Droid <fufudadw@gmail.com>
2024-06-04 13:15:21 +0530

    Update scripts

--- bubu-scripts/mirdate.sh
+++ bubu-scripts/mirdate.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # check https://www.reddit.com/r/archlinux/comments/3e7lq6/do_you_use_reflector_to_manage_your_mirror_list/
 # for further references
 
@@ -5,4 +7,4 @@
 # process, but the ideology is to run the command with -l 200 (which might 
 # take quite a bit of time) for one single time and never run it again
 
-reflector --verbose -l 200 -n 20 -p http,https --sort rate --save /etc/pacman.d/mirrorlist
+reflector --verbose -l 20 -n 20 -p http,https --sort rate --save /etc/pacman.d/mirrorlist
--- bubu-scripts/mktextemp.sh
+++ bubu-scripts/mktextemp.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Creating LaTeX template..."
 
 mkdir -p ./images
