fd290ab18fa36f7fa3a652473bfe5bcb9f0ab68b
Bubu-Droid <fufudadw@gmail.com>
2024-06-15 17:16:30 +0530

    Configure snippets

--- .config/nvim/lua/josean/plugins/vimtex.lua
+++ .config/nvim/lua/josean/plugins/vimtex.lua
@@ -4,5 +4,9 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here
+    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_mappings_enabled = 0
+    vim.g.vimtex_imaps_enabled = 0
+    vim.g.vimtex_quickfix_mode = 0
   end,
 }
