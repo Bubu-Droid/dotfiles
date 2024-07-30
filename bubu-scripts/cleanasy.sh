9e52fc23657de85c0f35fc4c71cbe1515b0a1299
Bubu-Droid <fufudadw@gmail.com>
2024-07-30 14:45:36 +0530

    Update scripts

--- bubu-scripts/cleanasy.sh
+++ bubu-scripts/cleanasy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-(python3 ~/.bubu-scripts/export-ggb-clean-asy.py < ./images/diag.txt) | python3 ~/.bubu-scripts/bubu-asy.py
+(python3 ~/bubu-scripts/export-ggb-clean-asy.py < ./images/diag.txt) | python3 ~/bubu-scripts/bubu-asy.py
 
 echo "GGB to ASY has successfully been cleaned!"
