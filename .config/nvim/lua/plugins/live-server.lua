802ee5ef70d325d53f0f1fbce58d5b0b3a5f14cf
Bubu-Droid <fufudadw@gmail.com>
2024-12-06 15:28:31 +0530

    Add md plugin

---
+++ .config/nvim/lua/plugins/live-server.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "barrett-ruth/live-server.nvim",
+    build = "npm add -g live-server",
+    cmd = { "LiveServerStart", "LiveServerStop" },
+    config = true,
+    keys = {
+      { "<leader>ls", "<cmd>LiveServerStart<CR>", desc = "Start live-server" },
+      { "<leader>lq", "<cmd>LiveServerStop<CR>", desc = "Stop live-server" },
+    },
+  },
+}
--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,7 +1,7 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install",
+  build = "cd app && npm install && git restore .",
   event = { "BufReadPre", "BufNewFile" },
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
