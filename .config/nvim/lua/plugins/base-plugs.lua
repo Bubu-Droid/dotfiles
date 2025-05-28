a0a45ddd1aedd4127e4663e6eed861b74a9bf62a
Bubu-Droid <fufudadw@gmail.com>
2025-05-28 21:00:19 +0530

    Fix bugs

--- .config/nvim/lua/plugins/base-plugs.lua
+++ .config/nvim/lua/plugins/base-plugs.lua
@@ -1,7 +1,19 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-  "nvim-tree/nvim-web-devicons",
+  {
+    "nvim-tree/nvim-web-devicons",
+    opts = {
+      default = true,
+      override = {
+        css = {
+          icon = "",
+          color = "#00BFFF",
+          name = "Css",
+        },
+      },
+    },
+  },
   "echasnovski/mini.icons",
   "williamboman/mason-lspconfig.nvim",
   { import = "plugins.lsp", event = { "UIEnter" } },
