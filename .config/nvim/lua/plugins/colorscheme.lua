cb6e40b6fe3754581c08af2f87030c554065554e
Bubu-Droid <fufudadw@gmail.com>
2025-09-14 02:55:40 +0530

    Set up automatic theme change

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,11 +4,64 @@ return {
   priority = 1000,
   opts = {},
   config = function()
-    require("tokyonight").setup({
-      style = "night",
-    })
+    local rice_file = os.getenv("HOME") .. "/.rice"
+    local theme_name = "tokyonight" -- fallback
+    local f = io.open(rice_file, "r")
+    if f then
+      theme_name = f:read("*l") -- read the first line
+      f:close()
+    end
 
-    -- load the colorscheme here
-    vim.cmd([[colorscheme tokyonight-night]])
+    if theme_name == "coolnight" then
+      local transparent = false -- set to true if you would like to enable transparency
+      local bg = "#011628"
+      local bg_dark = "#011423"
+      local bg_highlight = "#143652"
+      local bg_search = "#0A64AC"
+      local bg_visual = "#275378"
+      local fg = "#CBE0F0"
+      local fg_dark = "#B4D0E9"
+      local fg_gutter = "#627E97"
+      local border = "#547998"
+
+      require("tokyonight").setup({
+        style = "night",
+        transparent = transparent,
+        styles = {
+          sidebars = transparent and "transparent" or "dark",
+          floats = transparent and "transparent" or "dark",
+        },
+        on_colors = function(colors)
+          colors.bg = bg
+          colors.bg_dark = transparent and colors.none or bg_dark
+          colors.bg_float = transparent and colors.none or bg_dark
+          colors.bg_highlight = bg_highlight
+          colors.bg_popup = bg_dark
+          colors.bg_search = bg_search
+          colors.bg_sidebar = transparent and colors.none or bg_dark
+          colors.bg_statusline = transparent and colors.none or bg_dark
+          colors.bg_visual = bg_visual
+          colors.border = border
+          colors.fg = fg
+          colors.fg_dark = fg_dark
+          colors.fg_float = fg
+          colors.fg_gutter = fg_gutter
+          colors.fg_sidebar = fg_dark
+        end,
+      })
+      -- load the colorscheme here
+      vim.cmd("colorscheme tokyonight-night")
+    -- elseif theme_name == "coolnight" then
+    --   -- example for another theme plugin
+    --   require("coolnight").setup({ variant = "dark" })
+    --   vim.cmd("colorscheme coolnight-dark")
+    else
+      -- fallback / default
+      require("tokyonight").setup({
+        style = "night",
+      })
+      -- load the colorscheme here
+      vim.cmd("colorscheme tokyonight-night")
+    end
   end,
 }
