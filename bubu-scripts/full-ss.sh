28d509e8ea8704fd7116770f14b2da156e4a426e
Bubu-Droid <fufudadw@gmail.com>
2026-02-01 01:42:01 +0530

    Revert back to flameshot

--- bubu-scripts/full-ss.sh
+++ bubu-scripts/full-ss.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# flameshot full -c --path "$HOME/Pictures/screenshots"
-spectacle -b -c -f -n -o "$HOME/Pictures/screenshots/$(date '+%Y-%m-%d-%H%M%S')-$(shuf -i 1000-9999 -n 1).png"
+# spectacle -b -c -f -n -o "$HOME/Pictures/screenshots/$(date '+%Y-%m-%d-%H%M%S')-$(shuf -i 1000-9999 -n 1).png"
+flameshot full -c --path "$HOME/Pictures/screenshots"
 xrandr --output "$(xrandr | awk "/ connected/{print \$1; exit}")" --brightness 0.8
 sleep 0.25
 xrandr --output "$(xrandr | awk "/ connected/{print \$1; exit}")" --brightness 1
