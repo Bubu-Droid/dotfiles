7ecebb63fc43599897429ba1f013a4c6896a90a0
Bubu-Droid <fufudadw@gmail.com>
2026-03-01 02:15:20 +0530

    Use dict and add Rofi theme changer

--- bubu-scripts/theme.sh
+++ bubu-scripts/theme.sh
@@ -14,6 +14,15 @@
 # Some vars
 read -r current_rice <"$HOME/.rice"
 
+declare -A GTK_DICT
+declare -A POLY_DICT
+
+GTK_DICT["tokyonight"]="Tokyonight-Dark"
+GTK_DICT["coolnight"]="Tokyonight-Dark"
+
+POLY_DICT["tokyonight"]="F#8db0ff"
+POLY_DICT["coolnight"]="F#00BFFF"
+
 # List rices
 rices=$(find "$HOME/wallpapers/" -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | sort)
 
@@ -47,67 +56,26 @@ if [ -n "$selected" ] && [ "$selected" != "$current_rice" ]; then
   echo "$selected" >"$HOME"/.rice
 
   # Update Polybar theme
-  sed -i "$HOME/.config/polybar/config.ini" \
-    -e "s|\$HOME/.config/polybar/colors/.*\.ini|\$HOME/.config/polybar/colors/$selected.ini|"
-  if [ "$selected" = "tokyonight" ]; then
-    sed -i "$HOME/.config/polybar/modules/xkeyboard.ini" \
-      -e "s|%{F#[0-9a-fA-F]\{6\}}|%{F#8db0ff}|"
-  elif [ "$selected" = "coolnight" ]; then
-    sed -i "$HOME/.config/polybar/modules/xkeyboard.ini" \
-      -e "s|%{F#[0-9a-fA-F]\{6\}}|%{F#00BFFF}|"
-  else
-    sed -i "$HOME/.config/polybar/modules/xkeyboard.ini" \
-      -e "s|%{F#[0-9a-fA-F]\{6\}}|%{F#8db0ff}|"
-  fi
+  sed -Ei "s|polybar/colors/[^/]+\.ini|polybar/colors/$selected.ini|g" "$HOME/.config/polybar/config.ini"
+  sed -Ei "s|%\{F#[0-9a-fA-F]{6}\}|%{${POLY_DICT[$selected]}}|g" "$HOME/.config/polybar/modules/xkeyboard.ini"
 
   # Update Alacritty theme
-  sed -i "$HOME/.config/alacritty/alacritty.toml" \
-    -e "s|/home/bubu/.config/alacritty/themes/.*\.toml|/home/bubu/.config/alacritty/themes/$selected.toml|"
+  sed -Ei "s|themes/[^/]+\.toml|themes/$selected.toml|g" "$HOME/.config/alacritty/alacritty.toml"
 
   # Update Rofi theme
-  if [ "$selected" = "tokyonight" ]; then
-    sed -i "$HOME/.config/rofi/config.rasi" \
-      -e 's|background:[[:space:]]*#[0-9a-fA-F]\+;|background: #15161E;|'
-    sed -i "$HOME/.config/rofi/config.rasi" \
-      -e 's|background-alt:[[:space:]]*#[0-9a-fA-F]\+;|background-alt: #1A1B26;|'
-    sed -i "$HOME/.config/rofi/config.rasi" \
-      -e 's|bg/[a-zA-Z0-9_.-]\+|bg/tokyonight-bg.webp|'
-  elif [ "$selected" = "coolnight" ]; then
-    sed -i "$HOME/.config/rofi/config.rasi" \
-      -e 's|background:[[:space:]]*#[0-9a-fA-F]\+;|background: #11092D;|'
-    sed -i "$HOME/.config/rofi/config.rasi" \
-      -e 's|background-alt:[[:space:]]*#[0-9a-fA-F]\+;|background-alt: #281657;|'
-    sed -i "$HOME/.config/rofi/config.rasi" \
-      -e 's|bg/[a-zA-Z0-9_.-]\+|bg/coolnight-bg.png|'
-  else
-    sed -i "$HOME/.config/rofi/config.rasi" \
-      -e 's|background:[[:space:]]*#[0-9a-fA-F]\+;|background: #15161E;|'
-    sed -i "$HOME/.config/rofi/config.rasi" \
-      -e 's|background-alt:[[:space:]]*#[0-9a-fA-F]\+;|background-alt: #1A1B26;|'
-    sed -i "$HOME/.config/rofi/config.rasi" \
-      -e 's|bg/[a-zA-Z0-9_.-]\+|bg/tokyonight-bg.webp|'
-  fi
+  sed -Ei "s|themes/[^/]+\.rasi|themes/$selected.rasi|g" "$HOME/.config/rofi/config.rasi"
+  sed -Ei "s|themes/[^/]+\.rasi|themes/$selected.rasi|g" "$HOME/.config/rofi/powermenu.rasi"
+  sed -Ei "s|themes/[^/]+\.rasi|themes/$selected.rasi|g" "$HOME/.config/rofi/selector.rasi"
+  sed -Ei "s|images/[^/]+\.webp|images/$selected-bg.webp|g" "$HOME/.config/rofi/config.rasi"
+  sed -Ei "s|images/[^/]+\.webp|images/$selected-bg.webp|g" "$HOME/.config/rofi/powermenu.rasi"
 
   # Update wallpaper in i3 config
-  sed -i "$HOME/.config/i3/config" \
-    -e "s|\$HOME/wallpapers/$current_rice/.*\.png|\$HOME/wallpapers/$selected/01.png|"
+  sed -Ei "s|wallpapers/[^/]+/([^/]+)\.png|wallpapers/$selected/\1.png|g" "$HOME/.config/i3/config"
 
   # Update GTK theme
-  if [ "$selected" = "tokyonight" ] || [ "$selected" = "coolnight" ]; then
-    sed -i "$HOME/.config/gtk-3.0/settings.ini" \
-      -e "s|^gtk-theme-name=.*|gtk-theme-name=Tokyonight-Dark|"
-    sed -i "$HOME/.config/gtk-4.0/settings.ini" \
-      -e "s|^gtk-theme-name=.*|gtk-theme-name=Tokyonight-Dark|"
-    sudo sed -i "/etc/environment" \
-      -e "s|^GTK_THEME=.*|GTK_THEME=Tokyonight-Dark|"
-  else
-    sed -i "$HOME/.config/gtk-3.0/settings.ini" \
-      -e "s|^gtk-theme-name=.*|gtk-theme-name=Tokyonight-Dark|"
-    sed -i "$HOME/.config/gtk-4.0/settings.ini" \
-      -e "s|^gtk-theme-name=.*|gtk-theme-name=Tokyonight-Dark|"
-    sudo sed -i "/etc/environment" \
-      -e "s|^GTK_THEME=.*|GTK_THEME=Tokyonight-Dark|"
-  fi
+  sed -Ei "s|gtk-theme-name=.*|gtk-theme-name=${GTK_DICT[$selected]}|g" "$HOME/.config/gtk-3.0/settings.ini"
+  sed -Ei "s|gtk-theme-name=.*|gtk-theme-name=${GTK_DICT[$selected]}|g" "$HOME/.config/gtk-4.0/settings.ini"
+  sudo sed -Ei "s|GTK_THEME=.*|GTK_THEME=${GTK_DICT[$selected]}|g" "/etc/environment"
 
   # Restart i3
   i3-msg restart
