30ba501e93d5f6bfa1b72ef63a8a4c346ed85633
Bubu-Droid <fufudadw@gmail.com>
2026-03-04 23:29:38 +0530

    Update interface on toggling WiFi

--- .config/polybar/poly-scripts/toggle-wifi.sh
+++ .config/polybar/poly-scripts/toggle-wifi.sh
@@ -2,6 +2,10 @@
 
 if [ "$(nmcli radio wifi | grep "enabled" | wc -c)" -eq 0 ]; then
   nmcli radio wifi on
+  sed -Ei "s|interface[[:space:]]*=[[:space:]]*[a-zA-Z0-9_]*|interface = wlp2s0|g" "$HOME/.config/polybar/modules/network.ini"
+  i3-msg restart
 else
   nmcli radio wifi off
+  sed -Ei "s|interface[[:space:]]*=[[:space:]]*[a-zA-Z0-9_]*|interface = enp1s0|g" "$HOME/.config/polybar/modules/network.ini"
+  i3-msg restart
 fi
