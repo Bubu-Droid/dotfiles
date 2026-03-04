63a3a477c04912c1056db8f2cdc28573e806ad91
Bubu-Droid <fufudadw@gmail.com>
2026-03-05 00:04:57 +0530

    Update interface on toggling WiFi

--- .config/polybar/poly-scripts/toggle-wifi.sh
+++ .config/polybar/poly-scripts/toggle-wifi.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+read -r current_device <"$HOME/.device"
+
 if [ "$(nmcli radio wifi | grep "enabled" | wc -c)" -eq 0 ]; then
   nmcli radio wifi on
-  sed -Ei "s|interface[[:space:]]*=[[:space:]]*[a-zA-Z0-9_]*|interface = wlp2s0|g" "$HOME/.config/polybar/modules/network.ini"
-  i3-msg restart
+  if [ "$current_device" == "laptop" ]; then
+    sed -Ei "s|interface[[:space:]]*=[[:space:]]*[a-zA-Z0-9_]*|interface = wlp2s0|g" "$HOME/.config/polybar/modules/network.ini"
+  fi
 else
   nmcli radio wifi off
-  sed -Ei "s|interface[[:space:]]*=[[:space:]]*[a-zA-Z0-9_]*|interface = enp1s0|g" "$HOME/.config/polybar/modules/network.ini"
-  i3-msg restart
+  if [ "$current_device" == "laptop" ]; then
+    sed -Ei "s|interface[[:space:]]*=[[:space:]]*[a-zA-Z0-9_]*|interface = enp1s0|g" "$HOME/.config/polybar/modules/network.ini"
+  fi
 fi
+
+i3-msg restart
