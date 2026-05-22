3b13116f1c05e17423a1790ea7c51e0deee12692
Bubu-Droid <fufudadw@gmail.com>
2026-05-22 11:54:46 +0530

    Change indent char and add highlights

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -2,7 +2,32 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPre", "BufNewFile" },
   main = "ibl",
-  opts = {
-    indent = { char = "┊" },
-  },
+  -- opts = {},
+  config = function(_, opts)
+    local highlight = {
+      "Function",
+      "Conditional",
+      "Repeat",
+      "Label",
+      "Exception",
+    }
+    local ibl = require("ibl")
+
+    ibl.setup(opts)
+    ibl.setup({
+      indent = {
+        -- highlight = highlight,
+        char = "║",
+      },
+      whitespace = {
+        highlight = highlight,
+      },
+      scope = {
+        highlight = highlight,
+        include = {
+          node_type = { ["*"] = { "*" } },
+        },
+      },
+    })
+  end,
 }
