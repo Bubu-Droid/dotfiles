7c1e76f746a5d935fc5db2674558faeff3542151
Bubu-Droid <fufudadw@gmail.com>
2026-05-23 23:44:10 +0530

    Remove redundant comment

--- .config/nvim/lua/plugins/maximizer.lua
+++ .config/nvim/lua/plugins/maximizer.lua
@@ -2,7 +2,6 @@ return {
   "szw/vim-maximizer",
   event = { "BufReadPre", "BufNewFile" },
   keys = {
-    -- vim-maximizer keymaps
     { "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" },
   },
 }
