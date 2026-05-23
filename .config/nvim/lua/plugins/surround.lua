ad7dc6229918cba5d18e0a509e5f4cdb67a7816e
Bubu-Droid <fufudadw@gmail.com>
2026-05-23 23:41:58 +0530

    Add examples for ysiwf and ysiwi

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,5 +1,13 @@
 return {
   "kylechui/nvim-surround",
   event = "VeryLazy",
-  config = true,
+  opts = {
+    move_cursor = "sticky",
+  },
+
+  -- Old text                    Command         New text ~
+  -- arg*s                       ysiwffunc       func(args)
+  -- f*unc_name(a, b, x)         dsf             a, b, x
+  -- f*unc_name(a, b, x)         csfnew_name     new_name(a, b, x)
+  -- *inner text                  ys$i/<CR>\     /inner text\
 }
