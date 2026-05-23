be3594a268f4d767112010b5f3e70e9af599e78c
Bubu-Droid <fufudadw@gmail.com>
2026-05-23 23:43:54 +0530

    Use opts instead of .setup()

--- .config/nvim/lua/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,9 +1,5 @@
 return {
   "norcalli/nvim-colorizer.lua",
   event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local colorizer = require("colorizer")
-
-    colorizer.setup()
-  end,
+  opts = {},
 }
