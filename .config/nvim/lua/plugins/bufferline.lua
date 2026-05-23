6eb1e4fb8338a9d09022f05c89f3fa905ad887b0
Bubu-Droid <fufudadw@gmail.com>
2026-05-23 23:50:29 +0530

    Update bufferline tab styling

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,10 +1,20 @@
 return {
   "akinsho/bufferline.nvim",
-  event = { "UIEnter" },
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+  event = "UIEnter",
+  -- opts = {},
+  config = function()
+    local bufferline = require("bufferline")
+
+    bufferline.setup({
+      options = {
+        mode = "tabs",
+        separator_style = "slant",
+        diagnostics = "nvim_lsp",
+        diagnostics_update_on_event = true,
+        style_preset = {
+          bufferline.style_preset.no_italic,
+        },
+      },
+    })
+  end,
 }
