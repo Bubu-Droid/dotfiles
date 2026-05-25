232742379210db9f87fbc17f1617378a44608409
Bubu-Droid <fufudadw@gmail.com>
2026-05-25 21:32:27 +0530

    Add lualine marker for pending mason updates

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,9 +1,35 @@
 return {
   "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   event = "UIEnter",
+
   config = function()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status")
+
+    -- function to count the number of pending updates in Mason
+    local function lualine_mason_updates()
+      local registry = require("mason-registry")
+      local installed_packages = registry.get_installed_package_names()
+      local packages_outdated = 0
+
+      for _, pkg_name in ipairs(installed_packages) do
+        local pkg = registry.get_package(pkg_name)
+        if pkg then
+          local ok_installed, installed_version = pcall(pkg.get_installed_version, pkg)
+          local ok_latest, latest_version = pcall(pkg.get_latest_version, pkg)
+          if ok_installed and ok_latest and installed_version ~= latest_version then
+            packages_outdated = packages_outdated + 1
+          end
+        end
+      end
+
+      if packages_outdated > 0 then
+        return "" .. " " .. packages_outdated
+      else
+        return ""
+      end
+    end
 
     local theme_name = "tokyonight"
     local my_lualine_theme
@@ -71,7 +97,19 @@ return {
         -- ignore_focus = { "NvimTree" },
       },
       sections = {
+        lualine_c = {
+          { "filename" },
+          {
+            function()
+              return require("auto-session.lib").current_session_name(true)
+            end,
+          },
+        },
         lualine_x = {
+          {
+            lualine_mason_updates,
+            color = { fg = "#56B6C2" },
+          },
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
@@ -81,14 +119,6 @@ return {
           -- { "encoding" },
           -- { "fileformat" },
           { "filetype" },
-        },
-        lualine_c = {
-          { "filename" },
-          {
-            function()
-              return require("auto-session.lib").current_session_name(true)
-            end,
-          },
         },
       },
       extensions = {
