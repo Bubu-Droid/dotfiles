ba482d0753003316cc1996579d2537295e842186
Bubu-Droid <fufudadw@gmail.com>
2026-05-25 21:26:07 +0530

    Add more examples of witty surround usage

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -9,5 +9,6 @@ return {
   -- arg*s                       ysiwffunc       func(args)
   -- f*unc_name(a, b, x)         dsf             a, b, x
   -- f*unc_name(a, b, x)         csfnew_name     new_name(a, b, x)
-  -- *inner text                  ys$i/<CR>\     /inner text\
+  -- *inner text                 ys$i/<CR>\      /inner text\
+  -- *x                          ysl"            "x"
 }
