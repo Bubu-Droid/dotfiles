9cb747eec5b52db94985226bd8aa11da537b75d3
Bubu-Droid <fufudadw@gmail.com>
2026-05-27 01:06:14 +0530

    Add chktex and clangtidy locally since Mason doesn't have them

--- .config/nvim/lua/plugins/lsp/linting.lua
+++ .config/nvim/lua/plugins/lsp/linting.lua
@@ -5,19 +5,89 @@ return {
   config = function()
     local lint = require("lint")
 
+    -- set up chktex
+    local function chktex_setup()
+      local pattern = "(%d+):(%d+):(%d+):(.+):(%d+):(.*)"
+      local severities = {
+        Error = vim.diagnostic.severity.ERROR,
+        Warning = vim.diagnostic.severity.WARN,
+        Message = vim.diagnostic.severity.INFO,
+      }
+
+      lint.linters.chktex = {
+        cmd = "chktex",
+        stdin = true,
+        args = { "-v0", "-I0", "-s", ":", "-f", "%l%b%c%b%d%b%k%b%n%b%m%b%b%b" },
+        parser = function(output, _)
+          local result = vim.fn.split(output, ":::")
+          local diagnostics = {}
+
+          for _, line in ipairs(result) do
+            local lineno, off, d, sev, code, desc = string.match(line, pattern)
+
+            lineno = tonumber(lineno or 1) - 1
+            off = tonumber(off or 1) - 1
+            d = tonumber(d or 1)
+            table.insert(diagnostics, {
+              source = "chktex",
+              lnum = lineno,
+              col = off,
+              end_lnum = lineno,
+              end_col = off + d,
+              message = desc,
+              severity = assert(severities[sev], "missing mapping for severity " .. sev),
+              code = code,
+              user_data = {
+                lsp = {
+                  code = code,
+                },
+              },
+            })
+          end
+          return diagnostics
+        end,
+      }
+    end
+
+    -- set up clangtidy
+    local function clangtidy_setup()
+      local pattern = [=[([^:]*):(%d+):(%d+): (%w+): ([^[]+) %[(.*)%]]=]
+      local groups = { "file", "lnum", "col", "severity", "message", "code" }
+
+      local severity_map = {
+        ["error"] = vim.diagnostic.severity.ERROR,
+        ["warning"] = vim.diagnostic.severity.WARN,
+        ["information"] = vim.diagnostic.severity.INFO,
+        ["hint"] = vim.diagnostic.severity.HINT,
+        ["note"] = vim.diagnostic.severity.HINT,
+      }
+
+      lint.linters.clangtidy = {
+        cmd = "clang-tidy",
+        stdin = false,
+        args = { "--quiet" },
+        ignore_exitcode = true,
+        parser = require("lint.parser").from_pattern(pattern, groups, severity_map, { ["source"] = "clang-tidy" }),
+      }
+    end
+
+    chktex_setup()
+    clangtidy_setup()
+
     lint.linters_by_ft = {
       html = { "htmlhint" },
       htmldjango = { "djlint" },
       css = { "stylelint" },
       javascript = { "eslint_d" },
       python = { "ruff" },
-      lua = { "selene" },
-      -- latex = { "chktex" },
+      -- lua = { "selene" },
+      tex = { "chktex" },
       sh = { "shellcheck" },
       markdown = { "markdownlint" },
-      -- c and cpp linting is handled by clangd itself
+      c = { "clangtidy" },
+      cpp = { "clangtidy" },
       -- we don't really need a linter for json
-      r = { "lintr" },
+      -- r = { "lintr" },
       yaml = { "yamllint" },
       -- we don't really need a linter for toml
     }
