573e1f15ce507b8e2ef80553678f68e16a146f8f
Bubu-Droid <fufudadw@gmail.com>
2026-06-01 20:10:14 +0530

    Use wired/wireless flags instead of interface name

--- .config/polybar/poly-scripts/toggle-wifi.sh
+++ .config/polybar/poly-scripts/toggle-wifi.sh
@@ -5,12 +5,12 @@ read -r current_device <"$HOME/.device"
 if [ "$(nmcli radio wifi | grep "enabled" | wc -c)" -eq 0 ]; then
   nmcli radio wifi on
   if [ "$current_device" == "laptop" ]; then
-    sed -Ei "s|interface[[:space:]]*=[[:space:]]*[a-zA-Z0-9_]*|interface = wlp2s0|g" "$HOME/.config/polybar/modules/network.ini"
+    sed -Ei "s|interface[[:space:]]*=[[:space:]]*[a-zA-Z0-9_]*|interface = wireless|g" "$HOME/.config/polybar/modules/network.ini"
   fi
 else
   nmcli radio wifi off
   if [ "$current_device" == "laptop" ]; then
-    sed -Ei "s|interface[[:space:]]*=[[:space:]]*[a-zA-Z0-9_]*|interface = enp1s0|g" "$HOME/.config/polybar/modules/network.ini"
+    sed -Ei "s|interface[[:space:]]*=[[:space:]]*[a-zA-Z0-9_]*|interface = ethernet|g" "$HOME/.config/polybar/modules/network.ini"
   fi
 fi
 
